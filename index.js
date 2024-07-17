57f192a78faf10c1375c110a2f2d6c24ab37558d
Aromakelapa <gmrom1404@gmail.com>
2024-07-17 11:13:44 +0700

    Update

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const fs = require('fs');
 
   const playlist = JSON.parse(fs.readFileSync('playlist.json', 'utf-8'));
   const author = playlist.author ? playlist.author.name : playlist.videos[0].author.name;
+  const forbid = /[\/\?<>\\:\*|"]/g;
   const title = playlist.title;
 
   for (let i in playlist.videos) {
     if (!fs.existsSync('downloads')) fs.mkdirSync('downloads');
     if (!fs.existsSync(`downloads/${author}`)) fs.mkdirSync(`downloads/${author}`);
     if (!fs.existsSync(`downloads/${author}/${title}`)) fs.mkdirSync(`downloads/${author}/${title}`);
-    if (fs.existsSync(`downloads/${author}/${title}/${playlist.videos[i].title}.mp3`)) continue;
+    if (fs.existsSync(`downloads/${author}/${title}/${playlist.videos[i].title.replace(forbid, '-')}.mp3`)) continue;
 
-    await download(playlist.videos[i].url, `downloads/${author}/${title}/${playlist.videos[i].title}.mp3`);
+    await download(playlist.videos[i].url, `downloads/${author}/${title}/${playlist.videos[i].title.replace(forbid, '-')}.mp3`);
   }
 
   console.log('\nDownloading playlist finished!');
